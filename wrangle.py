3cfa4004b9e5261440bd009302b38951a5ad4c17
Caitlyn Carney <caitlyn.r.carney@gmail.com>
2021-07-06 15:14:10 -0500

    Add flood cleaning funciton:

--- wrangle.py
+++ wrangle.py
@@ -7,14 +7,34 @@ import numpy as np
 
 def clean_air(df):
     '''Drops unneeded columns from the air quality df
-    then handles the nulls in alert triggered column'''
+    then handles the nulls in alert triggered column
+    set to date time format'''
     # drop the colums
     df = df.drop(columns=['LAT', 'LONG', 'Zone', 
                       'Sensor_id', 'SensorModel', 
                       'SensorStatus'])
+    # replace nulls in ALertTriggered to None
     df.fillna("None", inplace = True)
+    # set to date time format
+    df.DateTime = pd.to_datetime(df.DateTime)
     # return new df
     return df
 
+#-----------------------------------------------------------------------------
+
+# Flood Cleaning
+
+def clean_flood(df):
+    '''Drops unneeded columns from the med center flooding df
+    Makes sure DateTime is in DateTime format'''
+    # drop the colums
+    df = df.drop(columns=['LAT', 'LONG', 'Zone', 
+                          'Sensor_id', 'SensorModel', 
+                          'SensorStatus', 'AlertTriggered', 
+                          'Temp_C', 'Temp_F', 'Vendor'])
+    # Set to date time format
+    df.DateTime = pd.to_datetime(df.DateTime)
+    # return new df
+    return df
 
 #-----------------------------------------------------------------------------
