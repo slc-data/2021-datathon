b52ece3cf336b11cc38653cfe667bee72278f3e8
Lori Segovia <segovialori@gmail.com>
2021-07-15 00:53:51 -0500

    ran app and made edits

--- workbooks/lori/eda_app.py
+++ workbooks/lori/eda_app.py
@@ -11,44 +11,26 @@ from streamlit_pandas_profiling import st_profile_report
 # Web App Title
 st.markdown('''
 # **Explore COSA Data App**
-The **Explore COSA Data App** will take in csv's and output quick exploratory data analysis.
+The **Explore COSA Data App** will take in csv's and output quick exploratory data analysis using Pandas Profiling Report.
 ''')
 
 # Upload CSV data
 with st.sidebar.header('1. Upload your CSV data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file]()
-""")
+    uploaded_csv = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
 
 # Pandas Profiling Report
-if uploaded_file is not None:
+if uploaded_csv is not None:
     @st.cache
     def load_csv():
-        csv = pd.read_csv(uploaded_file)
+        csv = pd.read_csv(uploaded_csv)
         return csv
     df = load_csv()
     pr = ProfileReport(df, explorative=True)
-    st.header('**Input DataFrame**')
+    st.header('**Checkout The Data!**')
     st.write(df)
     st.write('---')
     st.header('**Pandas Profiling Report**')
     st_profile_report(pr)
 else:
-    st.info('Awaiting CSV file to be uploaded.')
-    if st.button('Press to use Example Dataset'):
-        # Example data
-        @st.cache
-        def load_data():
-            a = pd.DataFrame(
-                np.random.rand(100, 5),
-                columns=['a', 'b', 'c', 'd', 'e']
-            )
-            return a
-        df = load_data()
-        pr = ProfileReport(df, explorative=True)
-        st.header('**Input DataFrame**')
-        st.write(df)
-        st.write('---')
-        st.header('**Pandas Profiling Report**')
-        st_profile_report(pr)
+    st.info('Upload a COSA dataset :) ')
+
