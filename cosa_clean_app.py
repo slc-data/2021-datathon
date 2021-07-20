54ea9f6b9b37d90d4a2300d6858fb5b6a5f42eac
Lori Segovia <segovialori@gmail.com>
2021-07-19 22:52:38 -0500

    shell added for cleaning app

---
+++ cosa_clean_app.py
@@ -0,0 +1,15 @@
+
+# Streamlit CLEAN COSA DATA APP for Datathon
+
+# Imports
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+
+# Web App Title
+st.markdown('''
+# **Clean COSA Data App**
+The **Clean COSA Data App** will take in COSA csv's and wrangle it into a form that can be easily explored for data analysis!
+''')
+
